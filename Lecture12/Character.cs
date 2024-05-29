97d717bff2d50f7e20046273da3badb058abd7ce
joshuasaetern <JoshuaSaetern17@gmail.com>
2024-05-29 13:15:34 -0700

    Add project files.

---
+++ Lecture12/Character.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture12
+{
+    internal class Character
+    {
+        //Used for rolling stats
+        Random rand = new Random();
+
+        //Fields
+        String playerName;
+        String characterName;
+        int intelligence;
+        int constitution;
+        int proficiency;
+
+        //Constructors
+        public Character(String playerName, String characterName, int intelligence, int constitution)
+        {
+            this.playerName = playerName;
+            this.characterName = characterName;
+            this.intelligence = intelligence;
+            this.constitution = constitution;
+        }
+        public Character(String playerName, String characterName)
+        {
+            this.playerName= playerName;
+            this.characterName= characterName;
+            this.intelligence = rand.Next(0, 21);
+            this.constitution = rand.Next(0, 21);
+        }
+
+        //Properties
+        public string PlayerName { get => playerName; set => playerName = value; }
+        public string CharacterName { get => characterName; set => characterName = value; }
+        public int Intelligence { get => intelligence; set => intelligence = value; }
+        public int Constitution { get => constitution; set => constitution = value; }
+        public int Hacking { get => AbilityModifier(this.intelligence); }
+        public int WillPower { get => AbilityModifier(this.constitution); }
+
+        //Methods
+        public static int AbilityModifier(int stat)
+        {
+            //Makes it so that the Ability cannot be negative
+            if (stat < 10)
+            {
+                return 0;
+            }
+            
+            return (stat - 10) / 2;
+        }
+        public override string ToString()
+        {
+            return $"Player Name: {this.PlayerName}\n" +
+                   $"Character Name: {this.CharacterName}\n" +
+                   $"Intelligence: {this.Intelligence}\n" +
+                   $"Constitution: {this.Constitution}\n" +
+                   $"Hacking: {this.Hacking}\n" +
+                   $"Will Power: {this.WillPower}";
+        }
+
+    }
+}
---
+++ Lecture12/MainWindow.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lecture12
+{
+    /// Joshua Saetern
+    /// 05.29.2024
+    /// Computer Programming II
+    /// Lecture 12
+    public partial class MainWindow : Window
+    {
+        //List to hold Characters
+        List<Character> characters = new List<Character>();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            //Default Characters
+            characters.Add(new Character("Will", "McDurken", 14, 9));
+            characters.Add(new Character("Rafael", "Ragavan Quicksmith", 18, 16));
+            characters.Add(new Character("Charles", "Conan", 10, 0));
+
+            listViewCharacters.ItemsSource = characters;
+        }
+
+        private void btnDisplay_Click(object sender, RoutedEventArgs e)
+        {
+            if (listViewCharacters.SelectedIndex < 0)
+            {
+                MessageBox.Show("Select an item from the list");
+            }
+            else
+            {
+                //Using as keyword
+                Character tempCharacter = listViewCharacters.SelectedItem as Character;
+
+                //Check if as keyword worked
+                if (tempCharacter != null) {
+                    runDisplay.Text = tempCharacter.ToString();
+                }
+            }
+        }
+
+        private void btnCreateCharacter_Click(object sender, RoutedEventArgs e)
+        {
+            createCharacter();
+            listViewCharacters.Items.Refresh();
+        }
+
+        private bool createCharacter()
+        {
+            //Checks if the user has inputted text
+            if (String.IsNullOrEmpty(txtBoxUserPlayerName.Text))
+            {
+                MessageBox.Show("Please enter a Player Name");
+                return false;
+            }
+            if (String.IsNullOrEmpty(txtBoxUserCharacterName.Text))
+            {
+                MessageBox.Show("Please enter a Character Name");
+                return false;
+            }
+            else
+            {
+                characters.Add(new Character(txtBoxUserPlayerName.Text, txtBoxUserCharacterName.Text));
+                MessageBox.Show("Character Created");
+                return true;
+            }
+        }
+    }
+}
